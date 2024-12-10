12f3112c449db86221e035694a0ec49f646b8fc3
Miguel David Rodriguez <migueldavidrodriguez2@gmail.com>
2024-12-10 03:54:05 -0500

    dockerization

--- src/main/java/com/test/demo/DemoApplication.java
+++ src/main/java/com/test/demo/DemoApplication.java
@@ -9,10 +9,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+	
 
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- src/main/java/com/test/demo/services/ProductService.java
+++ src/main/java/com/test/demo/services/ProductService.java
@@ -4,10 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import com.test.demo.exceptions.ResourceNotFoundException;
 import com.test.demo.models.entity.Branch;
