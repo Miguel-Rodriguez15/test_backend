2b9d3d840e7a3aeecb5d036e8b99c8c6fec863a2
Miguel David Rodriguez <migueldavidrodriguez2@gmail.com>
2024-12-10 01:58:03 -0500

    function update name branch

--- src/main/java/com/test/demo/controllers/BranchController.java
+++ src/main/java/com/test/demo/controllers/BranchController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +24,11 @@ public class BranchController {
     public ResponseEntity<Branch> addBranch(@PathVariable Long franchiseId, @RequestBody Branch branch) {
       Branch newBranch = branchService.addBranch(franchiseId, branch);
         return new ResponseEntity<>(newBranch, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{branchId}/name")
+    public ResponseEntity<Branch> updateBranchName(@PathVariable Long branchId, @RequestBody String newName) {
+      Branch updatedBranch = branchService.updateBranchName(branchId, newName);
+      return ResponseEntity.ok(updatedBranch);
     }
 }
